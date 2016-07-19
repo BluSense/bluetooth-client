9e27d3ee7f1b42cb3c057a11a77e6bba497b33d7
Onnz <admin@ecobz.com>
2016-07-20 00:59:17 +0700

    Create bash install and update Howto

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo ""
+echo "--------------------------------------"
+echo "Welcome to Chula Bluetooth scan client"
+echo "--------------------------------------"
+echo ""
+echo "Enter your RaspPi device id :"
+read deviceid
+
+apt-get update
+apt-get upgrade
+apt-get install bluez python-bluez python-pip screen
+pip install unirest
+pip install requests
+mkdir /srv/bt_monitor
+mkdir /srv/bt_monitor/save
+mkdir /srv/bt_monitor/log
+cd /srv/bt_monitor
+echo $deviceid >> id.txt
+wget --no-check-certificate https://www.dropbox.com/s/09yd06khn694uss/bluetooth_scan.py
+wget --no-check-certificate https://www.dropbox.com/s/qvijrvyaf5tv7bb/bluetooth_scan_offline.py
+wget --no-check-certificate https://www.dropbox.com/s/ioyw88l2vzhukhh/async_datasend.py
+wget --no-check-certificate https://www.dropbox.com/s/oof4dishkqeaoc2/check_internet.py
+wget --no-check-certificate https://www.dropbox.com/s/x336zv8wwab2yxb/device_active.py
+wget --no-check-certificate https://www.dropbox.com/s/lq9vt3zs9qsuo3i/reboot_mr3020.py
+(crontab -u root -l; echo "@reboot /bin/sleep 60 ; /usr/bin/python /srv/bt_monitor/bluetooth_scan_offline.py ; /sbin/reboot" ) | crontab -u root -
+(crontab -u root -l; echo "@reboot /bin/sleep 75 ; /usr/bin/python /srv/bt_monitor/async_datasend.py" ) | crontab -u root -
+(crontab -u root -l; echo "*/1 * * * * /usr/bin/python /srv/bt_monitor/device_active.py" ) | crontab -u root -
+(crontab -u root -l; echo "*/4 * * * * /usr/bin/python /srv/bt_monitor/check_internet.py" ) | crontab -u root -
+(crontab -u root -l; echo "0 2 * * * /usr/bin/python /srv/bt_monitor/reboot_mr3020.py" ) | crontab -u root -
+(crontab -u root -l; echo "0 3 * * * /sbin/reboot" ) | crontab -u root -
+
+echo "Do you want to add Pi to Weaved (centralize control)? (y/n) :"
+read installweaved
+if [ $installweaved = y ]; then
+	apt-get install weavedconnectd
+	weavedinstaller
+fi
+
+echo "Finish install RaspPi ID : "
+echo $deviceid
+echo ""
+echo "Reboot ? (y/n) :"
+read isreboot
+
+if [ $isreboot = y ]; then
+	reboot
+fi
