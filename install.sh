9702861c0d3e02de49fed83f51a699100f73e73a
Kittipong H <kittipong@algorithmic.co.th>
2022-11-06 23:09:57 +0700

    Optional Install Shellhub

--- install.sh
+++ install.sh
@@ -48,6 +48,9 @@ read deviceid
 echo "Is connect to Industial 4G Router? (y/n)"
 read is_industialrouter
 
+echo "Install Shellhub? (y/n)"
+read is_shellhub
+
 echo "deb http://mirror.nus.edu.sg/raspbian/raspbian buster main contrib non-free rpi" > /etc/apt/sources.list
 cat /etc/apt/sources.list
 
@@ -107,30 +110,33 @@ printf "127.0.0.1\t$deviceid\n" | tee --append /etc/hosts
 hostnamectl set-hostname $deviceid
 systemctl restart avahi-daemon
 
-echo "Shellhub will now be installed CALL the following command..."
-INSTALL_URL="sh <(curl -Ss http://shellhub.blusense.co/install.sh?tenant_id=db1bdec8-fae7-4f8b-8556-2da8bf8f4d14&preferred_hostname=$deviceid&keepalive_interval=5)"
-echo "$INSTALL_URL"
+if [ $is_shellhub = n ]; then
+	echo "Shellhub will now be installed CALL the following command..."
+	INSTALL_URL="sh <(curl -Ss http://shellhub.blusense.co/install.sh?tenant_id=db1bdec8-fae7-4f8b-8556-2da8bf8f4d14&preferred_hostname=$deviceid&keepalive_interval=5)"
+	echo "$INSTALL_URL"
 
-docker run -d \
-       --name=shellhub \
-       --restart=on-failure \
-       --privileged \
-       --net=host \
-       --pid=host \
-       -v /:/host \
-       -v /dev:/dev \
-       -v /var/run/docker.sock:/var/run/docker.sock \
-       -v /etc/passwd:/etc/passwd \
-       -v /etc/group:/etc/group \
-       -v /etc/resolv.conf:/etc/resolv.conf \
-       -v /var/run:/var/run \
-       -v /var/log:/var/log \
-       -e SHELLHUB_SERVER_ADDRESS=http://shellhub.blusense.co \
-       -e SHELLHUB_PRIVATE_KEY=/host/etc/shellhub.key \
-       -e SHELLHUB_TENANT_ID=db1bdec8-fae7-4f8b-8556-2da8bf8f4d14 \
-       -e SHELLHUB_KEEPALIVE_INTERVAL=5 \
-       -e SHELLHUB_PREFERRED_HOSTNAME=$deviceid \
-       shellhubio/agent:v0.7.3
+	docker run -d \
+	       --name=shellhub \
+	       --restart=on-failure \
+	       --privileged \
+	       --net=host \
+	       --pid=host \
+	       -v /:/host \
+	       -v /dev:/dev \
+	       -v /var/run/docker.sock:/var/run/docker.sock \
+	       -v /etc/passwd:/etc/passwd \
+	       -v /etc/group:/etc/group \
+	       -v /etc/resolv.conf:/etc/resolv.conf \
+	       -v /var/run:/var/run \
+	       -v /var/log:/var/log \
+	       -e SHELLHUB_SERVER_ADDRESS=http://shellhub.blusense.co \
+	       -e SHELLHUB_PRIVATE_KEY=/host/etc/shellhub.key \
+	       -e SHELLHUB_TENANT_ID=db1bdec8-fae7-4f8b-8556-2da8bf8f4d14 \
+	       -e SHELLHUB_KEEPALIVE_INTERVAL=5 \
+	       -e SHELLHUB_PREFERRED_HOSTNAME=$deviceid \
+	       shellhubio/agent:v0.7.3
+fi
+
 echo "   ___  _         _       _     "
 echo "  / __\(_) _ __  (_) ___ | |__  "
 echo " / _\  | || '_ \ | |/ __|| '_ \ "
