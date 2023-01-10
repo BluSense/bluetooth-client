86784b9603d30b5f9764e3f0d2cb744db5b2f9c3
Kittipong H <kittipong@algorithmic.co.th>
2023-01-10 14:24:43 +0700

    fix DNS

--- install-orangepi4lts.sh
+++ install-orangepi4lts.sh
@@ -161,6 +161,8 @@ gateway 192.168.1.1
 dns-nameservers 192.168.1.1 8.8.8.8 8.8.4.4
 END
 
+echo 'nameserver 192.168.1.1' > /etc/resolv.conf
+
 echo "   ___  _         _       _     "
 echo "  / __\(_) _ __  (_) ___ | |__  "
 echo " / _\  | || '_ \ | |/ __|| '_ \ "
